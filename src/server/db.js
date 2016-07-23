1e9366bd9cd9b40fcd0de580221b698cf3724d2a
Matias Turunen <mt95@windowslive.com>
2016-07-23 19:52:08 +0300

    Import photosets from flickr

--- src/server/db.js
+++ src/server/db.js
@@ -10,16 +10,16 @@ export function insertPhotos(photos) {
   return db.task(t => {
     const queries = _.map(photos, photo =>
       db.none(template, {
-      id: photo.id,
-      owner: photo.owner,
-      secret: photo.secret,
-      server: photo.server,
-      farm: photo.farm,
-      title: photo.title,
-      date_taken: photo.datetaken,
-      lon: photo.longitude,
-      lat: photo.latitude,
-    }));
+        id: photo.id,
+        owner: photo.owner,
+        secret: photo.secret,
+        server: photo.server,
+        farm: photo.farm,
+        title: photo.title,
+        date_taken: photo.datetaken,
+        lon: photo.longitude,
+        lat: photo.latitude,
+      }));
 
     return t.batch(queries);
   });
@@ -27,21 +27,21 @@ export function insertPhotos(photos) {
 
 export function updatePhotos(photos) {
   const db = pgp(process.env.DATABASE_URL);
-  
+
   const template = 'UPDATE photos SET owner=$(owner), secret=$(secret), server=$(server), farm=$(farm), title=$(title), date_taken=$(date_taken), position=ST_GeographyFromText(\'SRID=4326;POINT($(lon) $(lat))\') WHERE id=$(id); ';
   return db.task(t => {
     const queries = _.map(photos, photo =>
       db.none(template, {
-      id: photo.id,
-      owner: photo.owner,
-      secret: photo.secret,
-      server: photo.server,
-      farm: photo.farm,
-      title: photo.title,
-      date_taken: photo.datetaken,
-      lon: photo.longitude,
-      lat: photo.latitude,
-    }));
+        id: photo.id,
+        owner: photo.owner,
+        secret: photo.secret,
+        server: photo.server,
+        farm: photo.farm,
+        title: photo.title,
+        date_taken: photo.datetaken,
+        lon: photo.longitude,
+        lat: photo.latitude,
+      }));
 
     return t.batch(queries);
   });
@@ -49,7 +49,7 @@ export function updatePhotos(photos) {
 
 export function upsertPhotos(photos) {
   const db = pgp(process.env.DATABASE_URL);
-  
+
   return db.query('SELECT id FROM photos;')
   .then(ids => _.map(ids, id => id.id))
   .then(ids => {
@@ -61,4 +61,78 @@ export function upsertPhotos(photos) {
       updatePhotos(toUpdate)
     );
   });
+}
+
+export function insertPhotosets(photosets) {
+  const db = pgp(process.env.DATABASE_URL);
+
+  const template = 'INSERT INTO photosets (id, title) VALUES ($(id), $(title)); ';
+
+  return db.task(t => {
+    const queries = _.map(photosets, photoset =>
+      db.none(template, {
+        id: photoset.id,
+        title: photoset.title._content,
+      })
+    );
+
+    return t.batch(queries);
+  });
+}
+
+export function updatePhotosets(photosets) {
+  const db = pgp(process.env.DATABASE_URL);
+
+  const template = 'UPDATE photosets SET title=$(title) WHERE id=$(id); ';
+
+  return db.task(t => {
+    const queries = _.map(photosets, photoset =>
+      db.none(template, {
+        id: photoset.id,
+        title: photoset.title._content,
+      })
+    );
+
+    return t.batch(queries);
+  });
+}
+
+export function upsertPhotosets(photosets) {
+  photosets = photosets.photosets.photoset;
+  getPhotosetIds()
+  .then(ids => {
+    const toCreate = _.filter(photosets, photoset => (ids.indexOf(photoset.id) === -1));
+    const toUpdate = _.filter(photosets, photoset => (ids.indexOf(photoset.id) !== -1));
+
+    return Promise.join(
+      insertPhotosets(toCreate),
+      updatePhotosets(toUpdate)
+    );
+  });
+}
+
+export function getPhotosetIds() {
+  const db = pgp(process.env.DATABASE_URL);
+
+  return db.query('SELECT id from photosets;')
+  .then(ids => _.map(ids, id => id.id));
+}
+
+export function reCreatePhotosetPhotos(photosetPhotos) {
+  const db = pgp(process.env.DATABASE_URL);
+
+  const template = 'INSERT INTO photoset_photos (photo_id, photoset_id) VALUES ($(photo_id), $(photoset_id))';
+  return db.none('TRUNCATE photoset_photos;')
+    .then(() => db.task(t => {
+      const queries = [];
+      _.forEach(photosetPhotos, photoset => {
+        _.forEach(photoset.photos, photoId => {
+          queries.push(db.none(template, {
+            photo_id: photoId,
+            photoset_id: photoset.photosetId,
+          }));
+        });
+      });
+      return t.batch(queries);
+    }));
 }
--- src/server/server.js
+++ src/server/server.js
@@ -17,14 +17,17 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Kuvakone'));
 
 app.get('/loadPhotos', (req, response) => {
+  const photosPerPage = 50;
   let flickr;
   Flickr.tokenOnly(flickrOptions, (error, flickrApp) => {
     flickr = flickrApp;
     getPublicPhotos()
-    .then(photos => {
-      return dbUtils.upsertPhotos(photos);
-    })
-    .then((a,b) => response.send({a,b}))
+    .then(photos => dbUtils.upsertPhotos(photos))          // update photos
+    .then(() => getPhotoSets())
+    .then(photosets => dbUtils.upsertPhotosets(photosets)) // update photosets
+    .then(() => getPhotoSetPhotoIds())
+    .then(photoIds => dbUtils.reCreatePhotosetPhotos(photoIds))   // map photos to photosets
+    .then(() => response.send('OK'))
     .catch(e => console.error(e));
   });
 
@@ -33,6 +36,7 @@ app.get('/loadPhotos', (req, response) => {
       user_id: flickrUserId,
       page: page || 1,
       extras: 'geo, date_taken',
+      per_page: photosPerPage,
     }, callback));
   }
 
@@ -41,14 +45,14 @@ app.get('/loadPhotos', (req, response) => {
       const photos = [];
       let pagesToGet;
       getPhotosCount()
-      .then(countPage => pagesToGet = Math.ceil(countPage.photos.total / 100))
+      .then(countPage => pagesToGet = Math.ceil(countPage.photos.total / photosPerPage))
       .then(() => {
         //console.log('pages to get', pagesToGet);
         const pagePromises = [];
         for (let i = 1; i <= pagesToGet; i++) {
           pagePromises.push(getPublicPhotosPage(i)
           .then(page => _.forEach(page.photos.photo, p => photos.push(p))));
-        };
+        }
         Promise.all(pagePromises).then(() => resolve(photos));
       });
     });
@@ -63,6 +67,49 @@ app.get('/loadPhotos', (req, response) => {
       }, callback));
   }
 
+  function getPhotoSets() {
+    return Promise.fromCallback(callback =>
+      flickr.photosets.getList({
+        user_id: flickrUserId,
+        per_page: 500,
+      }, callback));
+  }
+
+  function getPhotoSetPhotoIds() {
+    return dbUtils.getPhotosetIds()
+    .then(photosetIds => Promise.all(_.map(photosetIds, photosetId =>
+      getPhotoSetPhotoCount(photosetId)
+      .then(page => Math.ceil(page.photoset.total / photosPerPage))
+      .then(pagesToGet =>
+        Promise.all(_.map(_.range(1, pagesToGet + 1), pageNum =>
+          getPhotoSetPhotoPage(photosetId, pageNum)
+          .then(page => _.map(page.photoset.photo, p => p.id))
+        )))
+      .then(photoIds => ({ photosetId: photosetId, photos: _.flattenDeep(photoIds) }))
+
+    )));
+
+  }
+
+  function getPhotoSetPhotoCount(photoSetId) {
+    return Promise.fromCallback(callback =>
+      flickr.photosets.getPhotos({
+        user_id: flickrUserId,
+        per_page: 1,
+        photoset_id: photoSetId,
+        page: 1,
+      }, callback));
+  }
+
+  function getPhotoSetPhotoPage(photoSetId, page) {
+    return Promise.fromCallback(callback =>
+      flickr.photosets.getPhotos({
+        user_id: flickrUserId,
+        per_page: photosPerPage,
+        photoset_id: photoSetId,
+        page: page,
+      }, callback));
+  }
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
