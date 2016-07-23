d28520c010469c2811f86f258f346ae438ffd587
Miika Hänninen <miika.hanninen@reaktor.fi>
2016-07-23 14:20:27 +0300

    Refactor view code into components

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,51 +2,7 @@ import './styles.scss';
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Grid, Row, Col } from 'react-bootstrap';
-import superagent from 'superagent';
-import superagentAsPromised from 'superagent-as-promised';
 
-const request = superagentAsPromised(superagent);
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      photos: [],
-    };
-  }
-
-  componentWillMount() {
-    this.reloadPhotos();
-  }
-
-  reloadPhotos() {
-    request.get('/test/photos')
-      .accept('application/json')
-      .then(response => this.setState({ photos: response.body }));
-  }
-
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <Col>
-            <h1>Kuvakone</h1>
-          </Col>
-        </Row>
-        <Row>
-          {
-            this.state.photos.map(photo => (
-              <Col key={ photo.id } sm={ 2 }>
-                <img src={ photo.thumbnail } />
-              </Col>
-            ))
-          }
-        </Row>
-      </Grid>
-    );
-  }
-}
+import { App } from './components';
 
 render(<App />, document.getElementById('kuvakone'));
