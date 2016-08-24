d634072effaf7686564f8d62c926ce76a8b74a01
Miika Hänninen <miika.hanninen@gmail.com>
2016-08-24 23:32:49 +0300

    Move table creation to reset-database script

--- vagrant/reset-database.sh
+++ vagrant/reset-database.sh
@@ -3,3 +3,5 @@ sudo -u postgres psql -f vagrant/create-database-and-user.sql || exit 1
 
 sudo -u postgres psql -c 'CREATE EXTENSION postgis;' vagrant || exit 1
 sudo -u postgres psql -c 'CREATE EXTENSION postgis;' test || exit 1
+
+psql $DATABASE_URL -f sql/create-tables.sql
